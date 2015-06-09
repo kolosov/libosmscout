c2e6100e9111f3ff9f8ea07a38a1a57e80f20e7c
Sergey Kolosov <kolosov@gmail.com>
2015-06-09 11:42:28 +0300

    Fix qrc resource loading

--- osmwami/src/MainWindow.cpp
+++ osmwami/src/MainWindow.cpp
@@ -21,7 +21,8 @@
 
 MainWindow::MainWindow(const SettingsRef& settings,
                        DBThread *dbThread)
- : QQmlApplicationEngine(QUrl::fromLocalFile("qml/main.qml")),
+ //: QQmlApplicationEngine(QUrl::fromLocalFile("qml/main.qml")),
+   : QQmlApplicationEngine(QUrl("qrc:///qml/main.qml")),
    settings(settings),
    dbThread(dbThread)
 {
