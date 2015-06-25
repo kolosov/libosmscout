3c28e08bee2d7cf3ac40f3b363761c6817600f1b
Sergey Kolosov <kolosov@gmail.com>
2015-06-25 15:16:44 +0300

    Add POIMapPainter

--- osmwami/src/ownpoi.h
+++ osmwami/src/ownpoi.h
@@ -6,13 +6,23 @@
 #include "RoutingModel.h"
 #include "SearchLocationModel.h"
 
+#include <osmscout/Location.h>
+#include <osmscout/Route.h>
+#include <osmscout/Database.h>
+#include <osmscout/RoutingProfile.h>
+#include <osmscout/RoutingService.h>
+#include <osmscout/RoutePostprocessor.h>
+
+
 class OwnPOI : public QObject
 {
     Q_OBJECT
     Q_PROPERTY(Location* startLoc READ getStartLoc)
     Q_PROPERTY(Location* endLoc READ getEndLoc)
 public:
-    explicit OwnPOI(QObject *parent = 0);
+    OwnPOI();
+    //osmscout::TypeConfigRef GetTypeConfig() const;
+
     Q_INVOKABLE void LoadPOI();
     Location *startLoc;
     Location *endLoc;
@@ -23,6 +33,20 @@ public:
     Location *getEndLoc() {
         return endLoc;
     }
+
+    osmscout::DatabaseParameter   databaseParameter;
+    osmscout::DatabaseRef         database;
+    osmscout::StyleConfigRef      styleConfig;
+
+    osmscout::RouterParameter           routerParameter;
+    osmscout::RoutingServiceRef router;
+    osmscout::ObjectFileRef                   startObject;
+    size_t                                    startNodeIndex;
+
+    osmscout::ObjectFileRef                   targetObject;
+    size_t                                    targetNodeIndex;
+
+    osmscout::RouteData                 data;
 
 signals:
 
